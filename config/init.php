4574909e5e752086358cba79fcfe0080135b84ed
Wasym <46717121+wasymshykh@users.noreply.github.com>
2021-01-06 03:18:47 +0500

    initial project setup

---
+++ config/init.php
@@ -0,0 +1,35 @@
+<?php
+
+    session_start();
+    define('DIR', dirname(__DIR__).'/');
+    define('URL', "http://localhost/widget");
+
+    // Either: development/production
+    define('PROJECT_MODE', 'development'); 
+    
+    // Timezone setting
+    define('TIMEZONE', 'Asia/Karachi');
+    date_default_timezone_set(TIMEZONE);
+
+    if (PROJECT_MODE !== 'development') {
+        error_reporting(0);
+    } else {
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    }
+
+    // Database connection details
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'widget_db');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+
+    // Auto load classes
+    require_once DIR . 'config/auto_loader.php';
+    
+    // Functions
+    include DIR . 'includes/functions.php';
+    
+    // Get db handle
+    $db = (new DB())->connect();
